a299c17f92d2d58337e9be30194b3d727bccfe29
EthanTinsley <79554368+EthanTinsley@users.noreply.github.com>
2021-08-05 21:26:00 -0400

    Add files via upload

---
+++ CodeBase/src/main.py
@@ -0,0 +1,12 @@
+from frontend.checkwriter_application import CheckWriterApplication
+
+def main ():
+    # create application
+    root = CheckWriterApplication()
+
+    # run application
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
